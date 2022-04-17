4daaef29e6842e27b295b7113b4fe79bfb26e378
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-04-17 23:53:12 +0100

    Update universal.py

--- universal.py
+++ universal.py
@@ -7,6 +7,9 @@ import keyboard as kb
 import time 
 import admin
 import ctypes, sys
+import winapps
+
+
 
 windows = '''
 
@@ -46,8 +49,10 @@ windows = '''
 
 '''
 
+for item in winapps.search_installed('chrome'):
+    print(item)
 
-
+time.sleep(69)
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
@@ -64,12 +69,43 @@ if platform == 'Windows':
   print(f'OS Detected is {platform}')
   check = input('Is this correct? (y/n): ')
   if check == 'y':
+    amount = 10
+    print(f'Going to Install {amount} Applications')
     time.sleep(2)
     playsound('windows.mp3')
     print('')
+    os.system("Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))")
     print('Installing FireFox...')
     os.system('choco install firefox -y --force')
     print('FireFox Installed!')
+    print('Installing CCleaner...')
+    os.system('choco install ccleaner -y --force')
+    print('Ccleaner Installed...')
+    print('Installing Git...')
+    os.system('choco install git -y --force')
+    print('Git Installed!')
+    print('Installing Speccy...')
+    os.system('choco install speccy -y --force')
+    print('Speccy Installed!')
+    print('Installing Scrcpy')
+    os.system('choco install scrcpy -y --force')
+    print('Scrcpy Installed!')
+    print('Installing Discord...')
+    os.system('choco install discord -y --force')
+    print('Discord Installed!')
+    print('Installing VLC...')
+    os.system('choco install vlc -y --force')
+    print('VLC Installed!')
+    print('Installing Audacity...')
+    os.system('choco install audacity -y --force')
+    print('Audacity Installed!')
+    print('Installing OBS...')
+    os.system('choco install obs-studio -y --force')
+    print('OBS Installed!')
+    print('Installing Winrar...')
+    os.system('choco install winrar -y --force')
+    print('Installed Winrar!')
+
     time.sleep(5)
 
 
