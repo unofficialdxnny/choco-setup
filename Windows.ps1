ef8897b0aa1112adfe81b6fcd6bfeb1b7ba560b1
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-04-18 19:12:08 +0100

    Update Windows.ps1

--- Windows.ps1
+++ Windows.ps1
@@ -1,12 +1,139 @@
-# ChocoInstallBase.ps1 by Q (forked from atwork.at)
-# Get Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# See packages at https://chocolatey.org/packages/
-# Comment (add #) or uncomment (remove #) to include or exclude from your base install
-# Use according to your own needs...
 
 # Essentials
+choco install veracrypt -y
+choco install keepassxc -y
+choco install chromium -y
 choco install firefox -y
+choco install foxitreader -y
+choco install malwarebytes -y
+choco install ccleaner -y
+choco install f.lux -y
+choco install sudo -y
+choco install scrcpy -y
+choco install money-manager-ex -y
+choco install speccy -y
+choco install signal -y
+
+# Comms
+choco install skype -y
+choco install telegram.install -y
+choco install whatsapp -y
+
+# Media
+choco install vlc -y
+choco install audacity -y
+choco install deezer -y
+choco install openshot -y
+choco install obs-studio -y
+choco install zoomit -y
+
+# Design
+choco install gimp -y
+choco install inkscape -y
+
+# Additional Tools
+choco install 7zip -y
+choco install curl -y
+choco install youtube-dl -y
+choco install protonvpn -y
+choco install forticlientvpn -y
+
+# More Tools
+choco install powershell-core -y
+choco install sysinternals -y
+choco install powertoys -y
+
+# Windows Subsystem for Linux
+choco install wsl -y
+choco install wsl-debiangnulinux -y
+
+# Dev
+choco install putty -y
+choco install winscp -y
+choco install git -y
+choco install vscode -y 
+choco install postman -y
+choco install fiddler -y
+choco install heroku-cli -y
+choco install opencv -y
+choco install nodejs -y
+choco install python -y
+choco install yarn -y
+choco install velocity -y
+choco install mkcert -y
+
+# Android
+choco install android-sdk -y
+choco install gradle -y
+
+# Containers & Linux
+choco install docker-cli -y
+choco install docker-compose -y
+choco install docker-desktop -y
+choco install terraform -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
+#choco install minishift -y
+#choco install minikube -y
+choco install kubernetes-kompose -y
+choco install lens -y
+
+# Microsoft .NET Framework 4.7.2
+choco install netfx-4.7.1-devpack -y
+# or Microsoft .NET Framework 4.7.2
+choco install dotnetfx -y
+# or Microsoft .NET Framework 4.7.2 Developer Pack
+choco install netfx-4.7.2-devpack
+# Microsoft .NET Core 2.2.6
+choco install dotnetcore -y
+# or Microsoft .NET Core Runtime (Install) 2.2.6
+choco install dotnetcore-runtime.install -y
+
+# CTF Tools
+# Alternatively, pentestbox.org/#download has everything but no choco :(
+#choco install tor-browser -y
+#choco install burp-suite-free-edition -y
+#choco install wireshark -y
+#choco install exiftool -y
+#choco install apktool -y
+#choco install ghidra -y
+#choco install nmap -y
+#choco install fiddler -y
+
+# Games
+#choco install goggalaxy -y
+#choco install steam -y
+#choco install epicgameslauncher -y
+#choco install uplay -y
+#choco install origin -y
+#choco install retroarch -y
+#choco install pcsx2 -y
+#choco install dolphin -y
+
+# Game Tools
+#choco install ds4windows -y
+#choco install msiafterburner -y
+
+# Fonts
+#choco install cascadiafonts -y
+#choco install fira -y
+
+# Misc
+#choco install calibre -y
+#choco install moneymanagerex -y
+#choco install microsoft-windows-terminal -y
+#choco install qbittorrent -y
+#choco install winpcap -y
+#choco install yumi-uefi -y
+#choco install androidstudio -y
+#choco install blender -y
+#choco install mousewithoutborders -y
+#choco install speedtest -y
+#choco install rufus -y
+#choco install typora -y
 
 
+
+# You´re done. ;)
