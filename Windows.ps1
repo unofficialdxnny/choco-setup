dbf1a7730e4d7993251529a4326f2409a79e56f5
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2022-05-01 20:02:09 +0100

    Update Windows.ps1

--- Windows.ps1
+++ Windows.ps1
@@ -1,25 +1,34 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install firefox -y
-choco install ccleaner -y
-choco install speccy -y
-choco install telegram.install -y
-choco install vlc -y
-choco install audacity -y
-choco install obs-studio -y
-choco install youtube-dl -y
-choco install wsl -y
-choco install wsl-debiangnulinux -y
-choco install lens -y
-choco install tor-browser -y
-choco install burp-suite-free-edition -y
-choco install wireshark -y
-choco install nmap -y
-choco install steam -y
-choco install epicgameslauncher -y
-choco install origin -y
-choco install ds4windows -y
-choco install msiafterburner -y
-choco install microsoft-windows-terminal -y
-choco install blender -y
-choco install rufus -y
+choco install firefox -y --force
+choco install googlechrome -y --force
+choco install ccleaner -y --force
+choco install speccy -y --force
+choco install telegram.install -y --force
+choco install vlc -y --force
+choco install audacity -y --force
+choco install obs-studio -y --force
+choco install youtube-dl -y --force
+choco install wsl -y --force
+choco install wsl-debiangnulinux -y --force
+choco install lens -y --force
+choco install tor-browser -y --force
+choco install burp-suite-free-edition -y --force
+choco install wireshark -y --force
+choco install nmap -y --force
+choco install steam -y --force
+choco install epicgameslauncher -y --force
+choco install origin -y --force
+choco install ds4windows -y --force
+choco install msiafterburner -y --force
+choco install microsoft-windows-terminal -y --force
+choco install blender -y --force
+choco install rufus -y --force
+choco install git -y --force
+choco install adobereader -y --force
+choco install python3 -y --force
+choco install winrar -y --force
+choco install teamviewer -y --force
+choco install vscode -y --force
+choco install autoruns -y --force
+choco install nodejs -y --force 
